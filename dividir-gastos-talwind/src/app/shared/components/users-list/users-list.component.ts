7fb858294841d1112217bbfb328d9cd15a0bd2aa
Ivan Flores <ifloresmanc96@gmail.com>
2024-06-22 20:37:01 -0300

    ref: change user list as control value accessor

--- dividir-gastos-talwind/src/app/shared/components/users-list/users-list.component.ts
+++ dividir-gastos-talwind/src/app/shared/components/users-list/users-list.component.ts
@@ -1,10 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { NameValue } from '@app/models/view-models';
 
 @Component({
     selector: 'app-users-list',
     templateUrl: './users-list.component.html',
     styleUrls: ['./users-list.component.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: UsersListComponent,
+        },
+    ],
 })
-export class UsersListComponent {
-    @Input() userList: string[] = [];
+export class UsersListComponent implements ControlValueAccessor {
+    onChange = (users: NameValue<string>[]) => {};
+
+    onTouched = () => {};
+
+    touched = false;
+
+    disabled = false;
+
+    values: NameValue<string>[] = [];
+
+    writeValue(obj: []): void {
+        this.values = obj;
+    }
+    registerOnChange(fn: any): void {
+        this.onChange = fn;
+    }
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
+
+    setDisabledState?(isDisabled: boolean): void {
+        this.disabled = isDisabled;
+    }
+
+    remove(item: NameValue<string>) {
+        this.values = this.values.filter((value) => value !== item);
+        this.onChange(this.values);
+    }
 }
