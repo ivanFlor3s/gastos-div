75e521352dd19d96db15b73342ec96419be6b201
Ivan Flores <ifloresmanc96@gmail.com>
2024-07-15 23:45:42 -0300

    add spent item model

--- dividir-gastos-talwind/src/app/models/dtos/spents/index.ts
+++ dividir-gastos-talwind/src/app/models/dtos/spents/index.ts
@@ -1,1 +1,2 @@
 export * from './add-spent.dto';
+export * from './spent-item';
---
+++ dividir-gastos-talwind/src/app/models/dtos/spents/spent-item.ts
@@ -0,0 +1,8 @@
+export interface SpentItem {
+    id: number;
+    groupId: number;
+    amount: number;
+    description: string;
+    createdAt: Date;
+    spentMode: number;
+}
