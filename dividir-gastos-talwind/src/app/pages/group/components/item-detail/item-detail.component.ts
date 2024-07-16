70396fb7bc4c96db49a72be284d00fe02fe419f6
Ivan Flores <ifloresmanc96@gmail.com>
2024-07-15 23:47:46 -0300

    show spent items in group

--- dividir-gastos-talwind/src/app/pages/group/components/item-detail/item-detail.component.ts
+++ dividir-gastos-talwind/src/app/pages/group/components/item-detail/item-detail.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SpentItem } from '@app/models/dtos';
 
 @Component({
-  selector: 'app-item-detail',
-  templateUrl: './item-detail.component.html',
-  styleUrls: ['./item-detail.component.css']
+    selector: 'app-item-detail',
+    templateUrl: './item-detail.component.html',
+    styleUrls: ['./item-detail.component.css'],
 })
 export class ItemDetailComponent implements OnInit {
+    @Input() spent: SpentItem;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
