b5cca26c5f371148c261260efff49f027a910451
Ivan Flores <ifloresmanc96@gmail.com>
2024-07-17 23:15:29 -0300

    feat: add author + date payed on spent model - migrations

--- ApiGastos/ApiGastos/Entities/Spent.cs
+++ ApiGastos/ApiGastos/Entities/Spent.cs
@@ -7,15 +7,21 @@ namespace ApiGastos.Entities
         [Key]
         public int Id { get; set; }
         public int GroupId { get; set; }
+        [Required]
+        public string AuthorId { get; set; }
         
         [Required]
         [Range(0.0, Double.MaxValue, ErrorMessage = "El campo {0} debe ser mayor a {1}")]
         public decimal Amount { get; set; }
+        
+        [Required]
+        public DateTime PayedAt { get; set; }
 
         [MaxLength(50)]
         [Required]
         public string Description { get; set; }
         public SpentMode SpentMode { get; set; }
         public Group Group { get; set; } = null!;
+        public AppUser Author { get; set; }  
     }
 }
---
+++ ApiGastos/ApiGastos/Migrations/20240718020143_addSpentAuthorAndPayDate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiGastos.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSpentAuthorAndPayDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorId",
+                table: "Spent",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PayedAt",
+                table: "Spent",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Spent_AuthorId",
+                table: "Spent",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Spent_AspNetUsers_AuthorId",
+                table: "Spent",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Spent_AspNetUsers_AuthorId",
+                table: "Spent");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Spent_AuthorId",
+                table: "Spent");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Spent");
+
+            migrationBuilder.DropColumn(
+                name: "PayedAt",
+                table: "Spent");
+        }
+    }
+}
