5d7ff3d4f82744d62587acdba4dd927a951e9ecd
Ivan Flores <ifloresmanc96@gmail.com>
2024-07-28 13:13:55 -0300

    add mapper for ngbdate

--- dividir-gastos-talwind/src/core/utils/mapper.ts
+++ dividir-gastos-talwind/src/core/utils/mapper.ts
@@ -7,4 +7,18 @@ export class Mapper {
         }
         return new Date(date.year, date.month - 1, date.day);
     }
+    public static mapNgbDate(date: Date): NgbDate {
+        console.log(date);
+        if (!date) {
+            throw new Error('Date is required');
+        }
+        if (typeof date == 'string') {
+            date = new Date(date);
+        }
+        return new NgbDate(
+            date.getFullYear(),
+            date.getMonth() + 1,
+            date.getDate()
+        );
+    }
 }
