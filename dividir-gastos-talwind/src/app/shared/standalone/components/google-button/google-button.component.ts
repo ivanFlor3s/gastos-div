3736ee3747081b2b60ccb1182302ea8accc1223f
Ivan Flores <ifloresmanc96@gmail.com>
2024-08-19 22:15:12 -0300

    feat: add declare google btn sign in

---
+++ dividir-gastos-talwind/src/app/shared/standalone/components/google-button/google-button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-google-button',
+    templateUrl: './google-button.component.html',
+    styleUrls: ['./google-button.component.css'],
+    standalone: true,
+})
+export class GoogleButtonComponent implements OnInit {
+    @Input() disabled = false;
+    constructor() {}
+
+    ngOnInit() {}
+}
